f50600b2a940399bc9b36f1a18ca843c78751b35
sdnn44 <slidennnq@gmail.com>
2024-07-09 16:44:37 +0200

    Added scrollbar to the main view

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <div className="h-[50vh]"></div>
       <Projects setActiveMenu={setActiveMenu} />
       <div className="h-[50vh]"></div>
-      <p className="text-right p-4 text-[10px] text-yellow-500">Source: https://blog.olivierlarose.com/tutorials/mouse-image-distortion</p>
+      <p className="text-right p-4 text-[10px] text-yellow-500"><span className="font-bold">Source:</span> https://blog.olivierlarose.com/tutorials/mouse-image-distortion</p>
     </main>
   );
 }
